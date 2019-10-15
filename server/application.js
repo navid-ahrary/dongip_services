f377886471a2bafc65f0336f7280d89d7c4ad11c
navid_ahrary <navidarry@gmail.com>
2019-10-15 20:34:31 +0330

    use mongodb connector temporally

--- server/application.js
+++ server/application.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const path = require('path');
-const {BootMixin} = require('@loopback/boot');
-const {RepositoryMixin} = require('@loopback/repository');
-const {RestApplication} = require('@loopback/rest');
-const {ServiceMixin} = require('@loopback/service-proxy');
+const {
+  BootMixin
+} = require('@loopback/boot');
+const {
+  RepositoryMixin
+} = require('@loopback/repository');
+const {
+  RestApplication
+} = require('@loopback/rest');
+const {
+  ServiceMixin
+} = require('@loopback/service-proxy');
 const restExplorer = require('@loopback/rest-explorer');
 const MySequence = require('./sequence');
 
@@ -12,12 +20,14 @@ class Lb4Application extends BootMixin(ServiceMixin(RepositoryMixin(RestApplicat
   constructor(options = {}) {
     super(options);
 
-    this.route('get', '/hi', {responses: {}}, () => 'Hi!');
+    this.route('get', '/hi', {
+      responses: {}
+    }, () => 'Hi!');
 
     this.sequence(MySequence);
     this.static('/', path.join(__dirname, '../public'));
     this.bind(restExplorer.RestExplorerBindings.CONFIG).to({
-        path: '/explorer',
+      path: '/explorer',
     });
     this.component(restExplorer.RestExplorerComponent);
     this.projectRoot = __dirname;
--- server/sequence.js
+++ server/sequence.js
@@ -1,10 +1,14 @@
 'use strict';
 
+// eslint-disable-next-line no-undef
 module.exports = sequenceFactory({
   name: 'MySequence',
   handler: async function (context) {
     try {
-      const { request, response } = context;
+      const {
+        request,
+        response
+      } = context;
       const route = this.findRoute(request);
       const args = await this.parseParams(request, route);
       const result = await this.invoke(route, args);
