ec69e87c4e83ffe9d2e80582b67e9cab98240c46
navid_ahrary <navidarry@gmail.com>
2020-01-12 00:56:51 +0330

    my arango driver

--- src/repositories/blacklist.repository.ts
+++ src/repositories/blacklist.repository.ts
@@ -1,36 +1,16 @@
-import {DefaultKeyValueRepository} from '@loopback/repository';
-import {Blacklist} from '../models';
-import {RedisDataSource} from '../datasources';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {Blacklist, BlacklistRelations} from '../models';
+import {ArangodbDataSource} from '../datasources';
 import {inject} from '@loopback/core';
-import {promisify} from 'util';
-import {HttpErrors} from '@loopback/rest';
 
-export class BlacklistRepository extends DefaultKeyValueRepository<Blacklist> {
-  constructor(@inject('datasources.redisds') dataSource: RedisDataSource) {
+export class BlacklistRepository extends DefaultCrudRepository<
+  Blacklist,
+  typeof Blacklist.prototype.id,
+  BlacklistRelations
+> {
+  constructor(
+    @inject('datasources.arangodb') dataSource: ArangodbDataSource,
+  ) {
     super(Blacklist, dataSource);
-  }
-
-  execute = promisify(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (command: string, args: any[], callback: Function) => {
-      // eslint-disable-next-line no-invalid-this
-      const connector = this.kvModelClass.dataSource!.connector!;
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      connector.execute!(command, args, callback);
-    },
-  );
-
-  public async addTokenToBlacklist(userId: string, token: string) {
-    const result = await this.execute('SADD', [userId, token]);
-    return result;
-  }
-
-  public async checkTokenInBlacklist(userId: string, token: string) {
-    const result = await this.execute('SISMEMBER', [userId, token]);
-
-    if (result) {
-      throw new HttpErrors.Unauthorized('This token is blacklisted');
-    }
-    return;
   }
 }
