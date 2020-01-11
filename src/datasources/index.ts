56576ff2cf6cd4d10f3723382c9ab54cc60c2281
navid_ahrary <navidarry@gmail.com>
2020-01-11 18:59:47 +0330

    add neo4j  datasource

--- src/datasources/index.ts
+++ src/datasources/index.ts
@@ -1,2 +1,3 @@
 export * from './mongo.datasource';
 export * from './redis.datasource';
+export * from './neo-4-j.datasource';
---
+++ src/datasources/neo-4-j.datasource.ts
@@ -0,0 +1,36 @@
+import {
+  inject,
+  lifeCycleObserver,
+  LifeCycleObserver,
+  ValueOrPromise,
+} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+import config from './neo-4-j.datasource.config.json';
+
+@lifeCycleObserver('datasource')
+export class Neo4JDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'neo4j';
+
+  constructor(
+    @inject('datasources.config.neo4j', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+
+  /**
+   * Start the datasource when application is started
+   */
+  start(): ValueOrPromise<void> {
+    // Add your logic here to be invoked when the application is started
+  }
+
+  /**
+   * Disconnect the datasource when application is stopped. This allows the
+   * application to be shut down gracefully.
+   */
+  stop(): ValueOrPromise<void> {
+    return super.disconnect();
+  }
+}
