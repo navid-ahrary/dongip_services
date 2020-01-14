e0d8927d782ee2a394ba56a37d04b0bb070d303c
navid_ahrary <navidarry@gmail.com>
2020-01-14 15:16:59 +0330

    add _key property

--- src/models/blacklist.model.ts
+++ src/models/blacklist.model.ts
@@ -3,9 +3,17 @@ import { Entity, model, property } from '@loopback/repository';
 @model()
 export class Blacklist extends Entity {
   @property({
+    type: 'Number',
+    required: false,
+    generated: true,
+    id: true
+  })
+  _key: Number;
+
+  @property({
     type: 'string',
     required: true,
-    id: true,
+    id: false,
   })
   token: string;
 
