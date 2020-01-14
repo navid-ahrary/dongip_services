62efc2aa778cddf4903d31e3bbf29762a8334e2a
navid_ahrary <navidarry@gmail.com>
2020-01-15 00:28:11 +0330

    move blacklist to arangodb

--- src/repositories/blacklist.repository.ts
+++ src/repositories/blacklist.repository.ts
@@ -1,16 +1,26 @@
-import { DefaultCrudRepository } from '@loopback/repository';
+import { DefaultCrudRepository, Filter } from '@loopback/repository';
 import { Blacklist, BlacklistRelations } from '../models';
 import { ArangodbDataSource } from '../datasources';
 import { inject } from '@loopback/core';
+import { HttpErrors } from '@loopback/rest';
 
 export class BlacklistRepository extends DefaultCrudRepository<
   Blacklist,
-  typeof Blacklist.prototype.id,
+  typeof Blacklist.prototype._id,
   BlacklistRelations
   > {
   constructor(
     @inject('datasources.arangodb') dataSource: ArangodbDataSource,
   ) {
     super(Blacklist, dataSource);
+  }
+
+  public async checkTokenNotBlacklisted(filter: Filter): Promise<void> {
+    const exist = await this.find(filter);
+    if (exist.length === 0) {
+      return;
+    } else {
+      throw new HttpErrors.NotFound('Token is blacklisted!');
+    }
   }
 }
--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,11 +1,11 @@
-import {TokenService} from '@loopback/authentication';
-import {promisify} from 'util';
-import {TokenServiceBindings} from '../keys';
-import {inject} from '@loopback/core';
-import {UserProfile, securityId} from '@loopback/security';
-import {HttpErrors} from '@loopback/rest';
-import {repository} from '@loopback/repository';
-import {UsersRepository, BlacklistRepository} from '../repositories';
+import { TokenService } from '@loopback/authentication';
+import { promisify } from 'util';
+import { TokenServiceBindings } from '../keys';
+import { inject } from '@loopback/core';
+import { UserProfile, securityId } from '@loopback/security';
+import { HttpErrors } from '@loopback/rest';
+import { repository } from '@loopback/repository';
+import { UsersRepository, BlacklistRepository } from '../repositories';
 
 const jwt = require('jsonwebtoken');
 const signAsync = promisify(jwt.sign);
@@ -18,7 +18,7 @@ export class JWTService implements TokenService {
     @inject(TokenServiceBindings.TOKEN_SECRET) private jwtSecret: string,
     @inject(TokenServiceBindings.TOKEN_EXPIRES_IN) private jwtExpiresIn: string,
     @inject(TokenServiceBindings.TOKEN_ALGORITHM) private jwtAlgorithm: string,
-  ) {}
+  ) { }
 
   async verifyToken(accessToken: string): Promise<UserProfile> {
     if (!accessToken) {
@@ -31,15 +31,12 @@ export class JWTService implements TokenService {
       //decode user profile from token
       const decryptedToken = await verifyAsync(accessToken, this.jwtSecret);
 
-      await this.blacklistRepository.checkTokenInBlacklist(
-        decryptedToken.sub,
-        accessToken,
-      );
+      await this.blacklistRepository.checkTokenNotBlacklisted({ where: { token: accessToken } });
 
       // don't copy over  token field 'iat' and 'exp', nor 'email' to user profile
       userProfile = Object.assign(
-        {[securityId]: '', accountType: ''},
-        {[securityId]: decryptedToken.sub, accountType: decryptedToken.accountType},
+        { [securityId]: '', accountType: '' },
+        { [securityId]: decryptedToken.sub, accountType: decryptedToken.accountType },
       );
     } catch (error) {
       throw new HttpErrors.Unauthorized(`Error verifying access token: ${error.message}`);
