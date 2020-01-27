59bf8c9034bf6e4754db410595c36c9979611833
navid_ahrary <navidarry@gmail.com>
2020-01-28 03:12:46 +0330

    add UsersRels repository

---
+++ src/repositories/users-rels.repository.ts
@@ -0,0 +1,22 @@
+import { DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
+import { UsersRels, UsersRelsRelations, Users} from '../models';
+import { ArangodbDataSource } from '../datasources';
+import { inject, Getter} from '@loopback/core';
+import {UsersRepository} from './users.repository';
+
+export class UsersRelsRepository extends DefaultCrudRepository<
+  UsersRels,
+  typeof UsersRels.prototype._key,
+  UsersRelsRelations
+  > {
+
+  public readonly users: BelongsToAccessor<Users, typeof UsersRels.prototype._key>;
+
+  constructor(
+    @inject('datasources.arangodb') dataSource: ArangodbDataSource, @repository.getter('UsersRepository') protected usersRepositoryGetter: Getter<UsersRepository>,
+  ) {
+    super(UsersRels, dataSource);
+    this.users = this.createBelongsToAccessorFor('users', usersRepositoryGetter,);
+    this.registerInclusionResolver('users', this.users.inclusionResolver);
+  }
+}
