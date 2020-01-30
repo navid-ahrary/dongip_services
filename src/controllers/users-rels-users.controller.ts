937a421c422a73eaabcd1f531e290b057f8ab4e9
navid_ahrary <navidarry@gmail.com>
2020-01-31 01:52:12 +0330

    rename id to _key

--- src/controllers/users-rels-users.controller.ts
+++ src/controllers/users-rels-users.controller.ts
@@ -10,7 +10,7 @@ import {
   UsersRels,
   Users,
 } from '../models';
-import {UsersRelsRepository} from '../repositories';
+import { UsersRelsRepository } from '../repositories';
 
 export class UsersRelsUsersController {
   constructor(
@@ -18,21 +18,21 @@ export class UsersRelsUsersController {
     public usersRelsRepository: UsersRelsRepository,
   ) { }
 
-  @get('/users-rels/{id}/users', {
+  @get('/apis/users-rels/{_key}/users', {
     responses: {
       '200': {
         description: 'Users belonging to UsersRels',
         content: {
           'application/json': {
-            schema: {type: 'array', items: getModelSchemaRef(Users)},
+            schema: { type: 'array', items: getModelSchemaRef(Users) },
           },
         },
       },
     },
   })
   async getUsers(
-    @param.path.string('id') id: typeof UsersRels.prototype._key,
+    @param.path.string('_key') _key: typeof UsersRels.prototype._key,
   ): Promise<Users> {
-    return this.usersRelsRepository.users(id);
+    return this.usersRelsRepository.users(_key);
   }
 }
