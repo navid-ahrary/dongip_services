056f3b4f9cfbfc1a42c937fd56b905ed3c3dfeda
navid_ahrary <navidarry@gmail.com>
2020-02-02 03:06:58 +0330

    add

---
+++ src/controllers/virtual-users-users.controller.ts
@@ -0,0 +1,29 @@
+import { repository } from '@loopback/repository'
+import { param, get, getModelSchemaRef, } from '@loopback/rest'
+import { VirtualUsers, Users } from '../models'
+import { VirtualUsersRepository } from '../repositories'
+
+export class VirtualUsersUsersController {
+  constructor (
+    @repository( VirtualUsersRepository )
+    public virtualUsersRepository: VirtualUsersRepository,
+  ) { }
+
+  @get( '/virtual-users/{id}/users', {
+    responses: {
+      '200': {
+        description: 'Users belonging to VirtualUsers',
+        content: {
+          'application/json': {
+            schema: { type: 'array', items: getModelSchemaRef( Users ) },
+          },
+        },
+      },
+    },
+  } )
+  async getUsers (
+    @param.path.string( 'id' ) id: typeof VirtualUsers.prototype._key,
+  ): Promise<Users> {
+    return this.virtualUsersRepository.sourceUser( id )
+  }
+}
