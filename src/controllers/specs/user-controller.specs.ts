2e2dd244ba8391394213233f3c0822d8733620f2
navid_ahrary <navidarry@gmail.com>
2020-02-10 03:07:04 +0330

    del registration token

--- src/controllers/specs/user-controller.specs.ts
+++ src/controllers/specs/user-controller.specs.ts
@@ -1,12 +1,12 @@
 // TODO(jannyHou): This should be moved to @loopback/authentication
 export const UserProfileSchema = {
   type: 'object',
-  required: ['mobile', 'name'],
+  required: [ 'mobile', 'name' ],
   properties: {
-    mobile: {type: 'string'},
-    name: {type: 'string'},
+    mobile: { type: 'string' },
+    name: { type: 'string' },
   },
-};
+}
 
 // TODO(jannyHou): This is a workaround to manually
 // describe the request body of 'Users/login'.
@@ -15,25 +15,21 @@ export const UserProfileSchema = {
 
 const CredentialsSchema = {
   type: 'object',
-  required: ['phone', 'password', 'registerationToken'],
+  required: [ 'phone', 'password', 'registerationToken' ],
   properties: {
     phone: {
       type: 'string',
     },
     password: {
       type: 'string',
-      length: 4,
-    },
-    registerationToken: {
-      type: 'string',
     },
   },
-};
+}
 
 export const CredentialsRequestBody = {
   description: 'The input of login function',
   required: true,
   content: {
-    'application/json': {schema: CredentialsSchema},
+    'application/json': { schema: CredentialsSchema },
   },
-};
+}
