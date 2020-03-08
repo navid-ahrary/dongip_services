849578277d90a9f3bbe1719d7baa8668f0d948bd
navidarry <navidarry@gmail.com>
2020-03-08 10:11:01 +0000

    fix typo

--- src/repositories/verify.repository.ts
+++ src/repositories/verify.repository.ts
@@ -5,7 +5,7 @@ import {inject} from '@loopback/core';
 
 export class VerifyRepository extends DefaultCrudRepository<
   Verify,
-  typeof Verify.prototype._id,
+  typeof Verify.prototype._key,
   VerifyRelations
 > {
   constructor(@inject('datasources.arangodb') dataSource: ArangodbDataSource) {
