eb31e26e76cf411493e91ecbb92fab1c867e131c
navid <navidarry@gmail.com>
2020-03-08 13:23:13 +0330

    update service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,32 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { UserService } from '@loopback/authentication'
-import { Users, Credentials } from '../models'
-import { repository } from '@loopback/repository'
-import { UsersRepository, } from '../repositories'
-import { securityId } from '@loopback/security'
-import { HttpErrors } from '@loopback/rest'
+import {UserService} from '@loopback/authentication';
+import {Users, Credentials} from '../models';
+import {repository} from '@loopback/repository';
+import {UsersRepository} from '../repositories';
+import {securityId} from '@loopback/security';
+import {HttpErrors} from '@loopback/rest';
 
 export class MyUserService implements UserService<Users, Credentials> {
-  constructor (
-    @repository( UsersRepository ) public userRepository: UsersRepository,
-  ) { }
+  constructor(
+    @repository(UsersRepository) public userRepository: UsersRepository,
+  ) {}
 
-
-  isMatched ( str1: string, str2: string ) {
-    const isMatched = ( str1 === str2 ) ? true : false
-    return isMatched
+  isMatched(str1: string, str2: string) {
+    const isMatched = str1 === str2 ? true : false;
+    return isMatched;
   }
 
+  async verifyCredentials(credentials: Credentials): Promise<Users> {
+    const invalidCredentialsError = 'Invalid phone/password !';
 
-  async verifyCredentials ( credentials: Credentials ): Promise<Users> {
-    const invalidCredentialsError = 'Invalid phone/password !'
+    const foundUser = await this.userRepository.findOne({
+      where: {phone: credentials.phone},
+    });
 
-    const foundUser = await this.userRepository.findOne( {
-      where: { phone: credentials.phone },
-    } )
-
-    if ( !foundUser ) {
-      throw new HttpErrors.NotFound( invalidCredentialsError )
+    if (!foundUser) {
+      throw new HttpErrors.NotFound(invalidCredentialsError);
     }
 
     // const passwordMatched = this.isMatched(
@@ -38,17 +36,17 @@ export class MyUserService implements UserService<Users, Credentials> {
     //   throw new HttpErrors.Unauthorized( invalidCredentialsError )
     // }
 
-    return foundUser
+    return foundUser;
   }
 
-  convertToUserProfile ( user: Users ) {
-    if ( !user.phone ) {
-      throw new HttpErrors.Unauthorized( 'phone is null' )
+  convertToUserProfile(user: Users) {
+    if (!user.phone) {
+      throw new HttpErrors.Unauthorized('phone is null');
     }
 
     return {
-      [ securityId ]: user.getId(),
-      accountType: user.accountType
-    }
+      [securityId]: user.getId(),
+      accountType: user.accountType,
+    };
   }
 }
