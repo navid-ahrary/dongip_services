4d7a5769c8a0e5d30b7d62b7c8778d3aa7c1bf8a
navid <navidarry@gmail.com>
2020-03-08 12:34:02 +0330

    hashpassword return only hashed string

--- src/services/hash.password.bcryptjs.ts
+++ src/services/hash.password.bcryptjs.ts
@@ -1,6 +1,6 @@
-import { genSalt, hash, compare, } from 'bcryptjs'
-import { inject } from '@loopback/core'
-import { PasswordHasherBindings } from '../keys'
+import {genSalt, hash, compare} from 'bcryptjs';
+import {inject} from '@loopback/core';
+import {PasswordHasherBindings} from '../keys';
 
 /**
  * Service HashPassword using module 'bcryptjs'.
@@ -10,33 +10,36 @@ import { PasswordHasherBindings } from '../keys'
 export type HashPassword = (
   password: string,
   rounds: number,
-) => Promise<string>
+) => Promise<string>;
 // bind function to `services.bcryptjs.HashPassword`
-export async function hashPassword ( password: string, rounds: number )
-  : Promise<{ password: string, salt: string }> {
-  const salt = await genSalt( rounds )
-  return { password: await hash( password, salt ), salt: salt }
+export async function hashPassword(
+  password: string,
+  rounds: number,
+): Promise<string> {
+  const salt = await genSalt(rounds);
+  return hash(password, salt);
 }
 
 export interface PasswordHasher<T = string> {
-  hashPassword ( password: T ): Promise<{ password: string, salt: string }>
-  comparePassword ( providedPass: T, storedPass: T ): Promise<boolean>
+  hashPassword(password: T): Promise<string>;
+  comparePassword(providedPass: T, storedPass: T): Promise<boolean>;
 }
 
 export class BcryptHasher implements PasswordHasher<string> {
-  constructor (
-    @inject( PasswordHasherBindings.ROUNDS ) private readonly rounds: number,
-  ) { }
+  constructor(
+    @inject(PasswordHasherBindings.ROUNDS) private readonly rounds: number,
+  ) {}
 
-  async hashPassword ( password: string )
-    : Promise<{ password: string, salt: string }> {
-    const salt = await genSalt( this.rounds )
-    return { password: await hash( password, salt ), salt: salt }
+  async hashPassword(password: string): Promise<string> {
+    const salt = await genSalt(this.rounds);
+    return hash(password, salt);
   }
 
-  async comparePassword ( providedPass: string, storedPass: string )
-    : Promise<boolean> {
-    const passwordIsMatched = await compare( providedPass, storedPass )
-    return passwordIsMatched
+  async comparePassword(
+    providedPass: string,
+    storedPass: string,
+  ): Promise<boolean> {
+    const passwordIsMatched = await compare(providedPass, storedPass);
+    return passwordIsMatched;
   }
 }
