902c1603c7917a501fae3b9d3a297d9d929bc901
navid_ahrary <navidarry@gmail.com>
2020-03-09 19:11:30 +0330

    verify credentials method

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,52 +1,66 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import {UserService} from '@loopback/authentication';
-import {Users, Credentials} from '../models';
-import {repository} from '@loopback/repository';
-import {UsersRepository} from '../repositories';
-import {securityId} from '@loopback/security';
-import {HttpErrors} from '@loopback/rest';
+import {UserService} from '@loopback/authentication'
+import {repository} from '@loopback/repository'
+import {securityId} from '@loopback/security'
+import {HttpErrors} from '@loopback/rest'
+import {inject} from '@loopback/core'
+
+import {Users, Credentials} from '../models'
+import {UsersRepository, VerifyRepository} from '../repositories'
+import {PasswordHasherBindings} from '../keys'
+import {PasswordHasher} from '../services'
 
 export class MyUserService implements UserService<Users, Credentials> {
-  constructor(
+  constructor (
     @repository(UsersRepository) public userRepository: UsersRepository,
+    @repository(VerifyRepository) public verifyRepo: VerifyRepository,
+    @inject(PasswordHasherBindings.PASSWORD_HASHER)
+    public passwordHasher: PasswordHasher,
   ) {}
 
-  isMatched(str1: string, str2: string) {
-    const isMatched = str1 === str2 ? true : false;
-    return isMatched;
+  isMatched (str1: string, str2: string) {
+    return str1 === str2 ? true : false
   }
 
-  async verifyCredentials(credentials: Credentials): Promise<Users> {
-    const invalidCredentialsError = 'Invalid phone/password !';
+  async verifyCredentials (credentials: Credentials): Promise<Users> {
+    const invalidCredentialsError = 'Invalid phone/password !'
+
+    const foundVerify = await this.verifyRepo.findOne({
+      where: {
+        and: [
+          {
+            phone: credentials.phone
+          },
+          {
+            code: credentials.code
+          }
+        ]
+      }
+    })
+
+    if (!foundVerify) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError)
+    }
 
     const foundUser = await this.userRepository.findOne({
       where: {phone: credentials.phone},
-    });
+    })
 
     if (!foundUser) {
-      throw new HttpErrors.NotFound(invalidCredentialsError);
+      throw new HttpErrors.NotFound(invalidCredentialsError)
     }
 
-    // const passwordMatched = this.isMatched(
-    //   credentials.password,
-    //   foundUser.password,
-    // )
-
-    // if ( !passwordMatched ) {
-    //   throw new HttpErrors.Unauthorized( invalidCredentialsError )
-    // }
-
-    return foundUser;
+    return foundUser
   }
 
-  convertToUserProfile(user: Users) {
+  convertToUserProfile (user: Users) {
     if (!user.phone) {
-      throw new HttpErrors.Unauthorized('phone is null');
+      throw new HttpErrors.Unauthorized('phone is null')
     }
 
     return {
       [securityId]: user.getId(),
       accountType: user.accountType,
-    };
+    }
   }
 }
