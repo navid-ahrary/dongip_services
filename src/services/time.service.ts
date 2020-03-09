5161e40c4ce0d842485de49bb748c70d897a8fad
navid_ahrary <navidarry@gmail.com>
2020-03-09 19:10:58 +0330

    del now method

--- src/services/time.service.ts
+++ src/services/time.service.ts
@@ -1,24 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { bind, BindingScope } from '@loopback/core'
+import {bind, BindingScope} from '@loopback/core'
 import moment from 'moment'
 
-@bind( { scope: BindingScope.SINGLETON } )
+@bind({scope: BindingScope.SINGLETON})
 export class TimeService {
-  constructor () { }
+  constructor () {}
 
   /**
    * Time differential
    */
-  public TimeDiff ( startTime: any, endTime: any, format: any ) {
-    startTime = moment( startTime, 'YYYY-MM-DD HH:mm:ss' )
-    endTime = moment( endTime, 'YYYY-MM-DD HH:mm:ss' )
-    return endTime.diff( startTime, format )
-  }
-
-  /**
-   * now time
-   */
-  public now () {
-    return moment().format()
+  public TimeDiff (startTime: any, endTime: any, format: any) {
+    startTime = moment(startTime, 'YYYY-MM-DD HH:mm:ss')
+    endTime = moment(endTime, 'YYYY-MM-DD HH:mm:ss')
+    return endTime.diff(startTime, format)
   }
 }
