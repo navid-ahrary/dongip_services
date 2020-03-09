4150fa000441f33ffb94c019d056b73564daae6c
navid_ahrary <navidarry@gmail.com>
2020-03-09 21:35:08 +0330

    verify credentials

---
+++ src/services/verify.service.ts
@@ -0,0 +1,39 @@
+import {bind, BindingScope, inject} from '@loopback/core';
+import {HttpErrors} from '@loopback/rest';
+import {repository} from '@loopback/repository';
+
+import {VerifyRepository} from '../repositories';
+import {Credentials, Verify} from '../models';
+import {PasswordHasher} from '../services';
+import {PasswordHasherBindings} from '../keys';
+
+@bind({scope: BindingScope.SINGLETON})
+export class VerifyService {
+  constructor (
+    @inject(PasswordHasherBindings.PASSWORD_HASHER)
+    public passwordHasher: PasswordHasher,
+    @repository(VerifyRepository) public verifyRepo: VerifyRepository
+  ) {}
+
+  public async verifyCredentials (credentials: Credentials): Promise<Verify> {
+    const invalidCredentialsError = 'Invalid phone/password !';
+
+    const foundVerify = await this.verifyRepo.findOne({
+      where: {phone: credentials.phone},
+    });
+
+    if (!foundVerify) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError);
+    }
+
+    const isMatched = await this.passwordHasher.comparePassword(
+      credentials.password, foundVerify?.password
+    );
+
+    if (!isMatched) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError);
+    }
+
+    return foundVerify;
+  }
+}
