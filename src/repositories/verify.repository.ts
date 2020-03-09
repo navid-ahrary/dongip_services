330e8a64a78f764d7e1c0df26d46e22a2c4b7029
navid_ahrary <navidarry@gmail.com>
2020-03-09 19:11:43 +0330

    crete like human kind method

--- src/repositories/verify.repository.ts
+++ src/repositories/verify.repository.ts
@@ -1,14 +1,24 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Verify, VerifyRelations} from '../models';
-import {ArangodbDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import {DefaultCrudRepository, DataObject} from '@loopback/repository'
+import {Verify, VerifyRelations} from '../models'
+import {ArangodbDataSource} from '../datasources'
+import {inject} from '@loopback/core'
 
 export class VerifyRepository extends DefaultCrudRepository<
   Verify,
   typeof Verify.prototype._key,
   VerifyRelations
-> {
-  constructor(@inject('datasources.arangodb') dataSource: ArangodbDataSource) {
-    super(Verify, dataSource);
+  > {
+  constructor (@inject('datasources.arangodb') dataSource: ArangodbDataSource) {
+    super(Verify, dataSource)
+  }
+
+  /**
+* create model like a human being
+*/
+  public async createHumanKind (entity: DataObject<Verify>): Promise<Verify> {
+    const verify = await this.create(entity)
+    verify._id = verify._key[1]
+    verify._key = verify._key[0]
+    return verify
   }
 }
