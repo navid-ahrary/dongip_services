eaa35b69d3a4164cf9e7db180d0e6ffa90d175e2
navid_ahrary <navidarry@gmail.com>
2020-03-11 19:29:13 +0330

    if verifty found, remove that from database

--- src/services/verify.service.ts
+++ src/services/verify.service.ts
@@ -34,6 +34,9 @@ export class VerifyService {
       throw new HttpErrors.Unauthorized(invalidCredentialsError);
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    this.verifyRepo.delete(foundVerify);
+
     return foundVerify;
   }
 }
