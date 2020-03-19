54ddc695a9681df03659820b353a61996da34b1b
Navid Ahrary <n.ahrary@avakatan.com>
2020-03-19 14:34:55 +0330

    fix debug name

--- src/services/sms.service.ts
+++ src/services/sms.service.ts
@@ -2,30 +2,29 @@ import {bind, BindingScope} from '@loopback/core';
 const Kavenegar = require('kavenegar');
 
 import Debug from 'debug';
-const debug = Debug('dongip-sms');
+const debug = Debug('dongip');
 
 import {config} from 'dotenv';
 config();
 
 @bind({scope: BindingScope.TRANSIENT})
 export class SmsService {
-  constructor (
-  ) {
-  }
+  constructor() {}
 
-  smsApi = Kavenegar.KavenegarApi(
-    {apikey: process.env.KAVENEGAR_API});
+  smsApi = Kavenegar.KavenegarApi({apikey: process.env.KAVENEGAR_API});
 
-
-  public sendSms (template: string, token: string, receptor: string) {
-    this.smsApi.VerifyLookup({
-      token: token,
-      template: template,
-      type: 'sms',
-      receptor: receptor.replace('+98', '0')
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    }, function (_response: any, _status: any) {
-      debug(_response, _status);
-    });
+  public sendSms(template: string, token: string, receptor: string) {
+    this.smsApi.VerifyLookup(
+      {
+        token: token,
+        template: template,
+        type: 'sms',
+        receptor: receptor.replace('+98', '0'),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      },
+      function(_response: any, _status: any) {
+        debug(_response, _status);
+      },
+    );
   }
 }
