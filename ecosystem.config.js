653719a4f66c189fba2ff1b959b2345f1187c334
Navid Ahrary <root@185-73-114-195.nrp.co>
2020-03-23 12:23:50 -0400

    fix ecosystemc

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,19 @@
-/* eslint-disable @typescript-eslint/camelcase */
 module.exports = {
   apps: [{
-    name: 'dongip',
-    script: 'index.js',
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    max_memory_restart: '1G',
+    name: "dongip",
+    script: "./index.js",
     env: {
-      NODE_ENV: 'development'
+      NODE_ENV: "development"
+    },
+    env_test: {
+      NODE_ENV: "test",
+    },
+    env_staging: {
+      NODE_ENV: "staging",
     },
     env_production: {
-      NODE_ENV: 'production'
+      NODE_ENV: "production",
     }
-  }],
+  }]
+}
 
-  deploy: {
-    production: {
-      user: 'node',
-      host: '5.253.24.205',
-      ref: 'origin/master',
-      repo: 'git+https://gitlab.com/navid_ahrary/dongip_services.git',
-      path: '/var/www/production',
-      'post-deploy': 'yarn && pm2 reload ecosystem.config.js --env production'
-    }
-  }
-};
