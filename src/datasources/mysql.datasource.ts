e65195e622818ee72f04b4d1ef13d3d19430f008
Navid Ahrary <n.ahrary@avakatan.com>
2020-05-27 15:31:08 +0430

    move database connector info to env var

--- src/datasources/mysql.datasource.ts
+++ src/datasources/mysql.datasource.ts
@@ -7,12 +7,11 @@ dotenv.config();
 const config = {
   name: 'Mysql',
   connector: 'mysql',
-  url: '',
-  host: 'localhost',
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
-  database: 'dongip_db',
+  database: process.env.MYSQL_DBNAME,
 };
 
 @lifeCycleObserver('datasource')
