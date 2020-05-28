31f19d160545fe7c7a37bc567b02d5bf24180886
Navid Ahrary <n.ahrary@avakatan.com>
2020-05-28 12:23:17 +0430

    remove categoryBill api

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,4 +2,3 @@ export * from './users.controller';
 export * from './dongs.controller';
 export * from './users-categories.controller';
 export * from './users-users-rels.controller';
-export * from './users-rels-category-bill.controller';
