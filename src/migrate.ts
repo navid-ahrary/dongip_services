8997cd190a245c89062599c9f8534bedef6e76a8
Navid Ahrary <navidarry@gmail.com>
2020-08-24 12:03:04 +0430

    Set model orders for shema migration

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,21 +1,38 @@
-import { MyApplication } from './application'
+import {MyApplication} from './application';
 
-export async function migrate ( args: string[] ) {
-  const existingSchema = args.includes( '--rebuild' ) ? 'drop' : 'alter'
-  console.log( 'Migrating schemas (%s existing schema)', existingSchema )
+export async function migrate(args: string[]) {
+  const existingSchema = args.includes('--rebuild') ? 'drop' : 'alter';
+  console.log('Migrating schemas (%s existing schema)', existingSchema);
 
-  const app = new MyApplication()
-  await app.boot()
-  await app.migrateSchema( { existingSchema } )
+  const app = new MyApplication();
+  await app.boot();
+  await app.migrateSchema({
+    existingSchema,
+    models: [
+      'users',
+      'settings',
+      'notifications',
+      'messages',
+      'scores',
+      'categories',
+      'users_rels',
+      'virtual_users',
+      'groups',
+      'budgets',
+      'dongs',
+      'bill_list',
+      'payer_list',
+    ],
+  });
 
   // Connectors usually keep a pool of opened connections,
   // this keeps the process running even after all work is done.
   // We need to exit explicitly.
-  process.exit( 0 )
+  process.exit(0);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-migrate( process.argv ).catch( err => {
-  console.error( 'Cannot migrate database schema', err )
-  process.exit( 1 )
-} )
+migrate(process.argv).catch((err) => {
+  console.error('Cannot migrate database schema', err);
+  process.exit(1);
+});
