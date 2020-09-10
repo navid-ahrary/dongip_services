34669552caf496000fe63bfac7f143154ce1b687
Navid Ahrary <navidarry@gmail.com>
2020-09-10 17:23:09 +0430

    Fix origin property length

--- src/models/scores.model.ts
+++ src/models/scores.model.ts
@@ -42,7 +42,7 @@ export class Scores extends Entity {
     required: true,
     mysql: {
       dataType: 'varchar',
-      dataLength: 10,
+      dataLength: 50,
       nullable: 'N',
     },
   })
