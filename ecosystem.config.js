b2562b4f843177f3aaef2579fa7fe01ce545b192
Navid Ahrary <navidarry@gmail.com>
2020-11-09 15:38:28 +0330

    Add date format for pm2 logs and enable watch mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,8 @@ module.exports = {
     script: "./index.js",
     instances: "max",
     exec_mode: "cluster",
-    watch: false,
+    watch: true,
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
     env: {
       NODE_ENV: "development"
     },
