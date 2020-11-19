11f4b389b51170523cc8a1255b8bcd729eef1096
Navid Ahrary <navidarry@gmail.com>
2020-11-19 16:35:19 +0330

    Fix

--- src/interceptors/joint-accounts.interceptor.ts
+++ src/interceptors/joint-accounts.interceptor.ts
@@ -297,7 +297,7 @@ export class JointAccountsInterceptor implements Provider<Interceptor> {
         if (!foundDong) {
           throw this.locMsg['DONG_NOT_VALID'][this.lang];
         } else if (jointAcc && jointAcc.userId !== this.userId) {
-          if (!foundDong.originDongId) {
+          if (foundDong.originDongId) {
             throw util.format(
               this.locMsg['JOINT_ADMIN_DELETE_DONG_ERROR'][this.lang],
               jointAcc.title,
