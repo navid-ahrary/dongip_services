534ecfa69ae6fa2254637ada75dde9520f352f54
navid <navid@localhost.localdomain>
2020-12-02 19:17:29 +0330

    Fix

--- src/interceptors/joint-accounts.interceptor.ts
+++ src/interceptors/joint-accounts.interceptor.ts
@@ -26,6 +26,7 @@ import {
 import { BatchMessage, FirebaseService, PhoneNumberService } from '../services';
 import { LocalizedMessages } from '../application';
 import { HttpErrors, RestBindings, Request } from '@loopback/rest';
+import { JointAccountSubscribes } from '../models';
 
 /**
  * This class will be bound to the application as an `Interceptor` during
@@ -294,11 +295,12 @@ export class JointAccountsInterceptor implements Provider<Interceptor> {
         const foundDong = foundDongs[0];
 
         const jointAcc = foundDong?.jointAccount;
+        let jointSub: JointAccountSubscribes | null = null;
 
         if (!foundDong) {
           throw this.locMsg['DONG_NOT_VALID'][this.lang];
         } else if (jointAcc && jointAcc.userId !== this.userId) {
-          const jointSub = await this.jointAccSubscRepo.findOne({
+          jointSub = await this.jointAccSubscRepo.findOne({
             where: { userId: this.userId, jointAccountId: jointAcc.getId() },
           });
           if (foundDong.originDongId && jointSub) {
@@ -311,7 +313,7 @@ export class JointAccountsInterceptor implements Provider<Interceptor> {
 
         const result = await next();
 
-        if (jointAcc) {
+        if (jointAcc && jointSub) {
           if (foundDong.originDongId) await this.dongRepo.deleteById(foundDong.originDongId);
 
           const jointAccountSubs = jointAcc.jointAccountSubscribes;
