42067fbe55a6b0ee1eb3e4ea8239d53fe491c6ae
Navid Ahrary <n.ahrary@avakatan.com>
2021-03-02 17:13:31 +0330

    Fix

--- src/controllers/messages.controller.ts
+++ src/controllers/messages.controller.ts
@@ -80,7 +80,7 @@ export class MessagesController {
         this.firebaseService.sendMultiCastMessage({
           tokens: users.map((u) => u.firebaseToken!),
           notification: {
-            title: `TicketId ${createdMsg.getId()} From '${this.name}', id ${this.userId}`,
+            title: `New ticket from '${this.name}', id ${this.userId}`,
             body: messageContent,
           },
         });
