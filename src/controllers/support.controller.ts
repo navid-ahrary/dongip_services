5a784560aa4dea6e0e5b41495f94f83cc80e4385
Navid Ahrary <navid.ahrary@outlook.com>
2021-03-16 00:57:11 +0330

    Clean Code

--- src/controllers/support.controller.ts
+++ src/controllers/support.controller.ts
@@ -229,9 +229,7 @@ export class SupportController {
         const setting = _.find(foundSettings, (s) => s.userId === targetUserId)!;
         const lang = setting.language;
 
-        const timezone = Ct.getTimezonesForCountry(region!)
-          ? Ct.getTimezonesForCountry(region!)[0].name
-          : 'Asia/Tehran';
+        const timezone = Ct.getTimezonesForCountry(region ?? 'IR')[0].name;
         const timestamp = Moment.tz(timezone).format('YYYY-MM-DDTHH:mm:ss+00:00');
 
         const savedMsg = await this.usersRepository.messages(targetUserId).create({
