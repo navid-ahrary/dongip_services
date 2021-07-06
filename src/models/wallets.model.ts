976ab6b1521238a1f8b2e1fcd51ca91118890fce
Navid Ahrary <navid.ahrary@outlook.com>
2021-07-06 21:17:51 +0430

    Revert "Update: If a wallet deleted, belongs dongs whould not delete"
    This reverts commit 454af7bb6bce97db946008dc12da75d0cbdef95e.

--- src/models/wallets.model.ts
+++ src/models/wallets.model.ts
@@ -12,7 +12,7 @@ import { Dongs } from './dongs.model';
         entityKey: 'id',
         foreignKey: 'userId',
         onUpdate: 'cascade',
-        onDelete: 'set null',
+        onDelete: 'cascade',
       },
     },
   },
