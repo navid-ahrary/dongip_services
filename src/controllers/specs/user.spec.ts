54a6b0ece5738e57f4d46f45db6a31756164b8f1
Navid Ahrary <navid.ahrary@outlook.com>
2021-12-09 21:27:32 +0330

    FIx update user

--- src/controllers/specs/user.spec.ts
+++ src/controllers/specs/user.spec.ts
@@ -19,6 +19,7 @@ export const UserPatchRequestBody = {
           'userAgent',
           'phoneLocked',
           'emailLocked',
+          'isCompleted',
           'deleted',
         ],
         optional: ['phone', 'email'],
